cae1107dfd2f516ae7558c91bd13000261697a71
George <jora.grintsevich@gmail.com>
2023-01-16 13:53:44 +0500

    Add project files.

---
+++ Dictionary-Translator/Controllers/TranslateConstroller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dictionary_Translator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TranslateConstroller : Controller
+    {
+        [HttpPost]
+        [Route("translate")]
+        public IActionResult Translate([FromForm] string valueToTranslate)
+        {
+            string data = valueToTranslate;
+            return Ok();
+        }
+    }
+}
